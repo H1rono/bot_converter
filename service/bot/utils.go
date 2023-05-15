2edcf1c6b11e77eec3e9e620ebdc98ca56c7b80a
motoki317 <motoki317@gmail.com>
2023-05-15 15:10:55 +0900

    fix updated go-traq

--- service/bot/utils.go
+++ service/bot/utils.go
@@ -1,20 +1,19 @@
 package bot
 
 import (
-	"github.com/antihax/optional"
 	"github.com/sapphi-red/go-traq"
 )
 
 // getChannelPath gets the path to the channel.
 func (h *Handlers) getChannelPath(channelID string) (string, error) {
-	c, _, err := h.api.ChannelApi.GetChannel(h.auth, channelID)
+	c, _, err := h.api.ChannelApi.GetChannel(h.auth, channelID).Execute()
 	if err != nil {
 		return "", err
 	}
-	if c.ParentId == nil {
+	if !c.ParentId.IsSet() {
 		return c.Name, nil
 	}
-	p, err := h.getChannelPath(*c.ParentId)
+	p, err := h.getChannelPath(*c.ParentId.Get())
 	if err != nil {
 		return "", err
 	}
@@ -23,22 +22,20 @@ func (h *Handlers) getChannelPath(channelID string) (string, error) {
 
 // postMessage posts message to the channel in which the event happened.
 func (h *Handlers) postMessage(e *messageCreatedEvent, message string) (*traq.Message, error) {
-	m, _, err := h.api.ChannelApi.PostMessage(h.auth, e.Message.ChannelID, &traq.ChannelApiPostMessageOpts{
-		PostMessageRequest: optional.NewInterface(traq.PostMessageRequest{
-			Content: message,
-			Embed:   true,
-		}),
-	})
-	return &m, err
+	embed := true
+	m, _, err := h.api.ChannelApi.PostMessage(h.auth, e.Message.ChannelID).PostMessageRequest(traq.PostMessageRequest{
+		Content: message,
+		Embed:   &embed,
+	}).Execute()
+	return m, err
 }
 
 // postDirectMessage posts message to the specified user.
 func (h *Handlers) postDirectMessage(userID string, message string) (*traq.Message, error) {
-	m, _, err := h.api.UserApi.PostDirectMessage(h.auth, userID, &traq.UserApiPostDirectMessageOpts{
-		PostMessageRequest: optional.NewInterface(traq.PostMessageRequest{
-			Content: message,
-			Embed:   true,
-		}),
-	})
-	return &m, err
+	embed := true
+	m, _, err := h.api.UserApi.PostDirectMessage(h.auth, userID).PostMessageRequest(traq.PostMessageRequest{
+		Content: message,
+		Embed:   &embed,
+	}).Execute()
+	return m, err
 }
